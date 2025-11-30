2855cda91c4416bf5a8c0e50006d5c5d489bf58e
raghur4567 <iamraghu1802@gmail.com>
2025-11-30 11:15:24 +0000

    ci github-actions

--- devops-challenge/app.py
+++ devops-challenge/app.py
@@ -7,6 +7,4 @@ def root():
     return jsonify({"message": "Hello, Candidate", "version": "1.0.0"})
 
 if __name__ == "__main__":
-    # IMPORTANT: listen on port 80
-    app.run(host="0.0.0.0", port=80)
-
+    app.run(host="0.0.0.0", port=5000)
